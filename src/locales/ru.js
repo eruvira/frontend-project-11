13fc756d06501384111a880c55ae492fedf1b999
Elvira Darisheva <d.elvira@rocketfirm.com>
2025-06-27 12:26:40 +0500

    feat: 3 step

---
+++ src/locales/ru.js
@@ -0,0 +1,16 @@
+export default {
+  translation: {
+    appName: 'RSS агрегатор',
+    form: {
+      inputPlaceholder: 'Ссылка на RSS',
+      submit: 'Добавить',
+      success: 'RSS успешно загружен',
+      errors: {
+        required: 'Не должно быть пустым',
+        url: 'Ссылка должна быть валидным URL',
+        duplicate: 'RSS уже существует',
+        network: 'Ошибка сети',
+      },
+    },
+  },
+};
